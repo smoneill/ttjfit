21e9c076ee35b31452a099256ea0d5def83a0a25
Sean O'Neill <smoneill@frontiernet.net>
2013-06-18 14:45:19 -0400

    update to scaling still needs work

--- ttjfit_trans.py
+++ ttjfit_trans.py
@@ -65,13 +65,19 @@ ttq1.Add(hists["ttqg"],hists["ttag"],eff["ttqg"],eff["ttag"])
 
 #Monte Carlo histogram array
 mc80 = r.TObjArray(3)
+sc = 2
+
+bg.Scale(sc)
+ttg.Scale(sc)
+ttq.Scale(sc)
+
 mc80.Add(bg)
 mc80.Add(ttg)
 mc80.Add(ttq)
 
 #Initializes the fitter
 fit0 = r.TFractionFitter(hists["data"],mc80)
-fit0.Constrain(1,0.095,0.105)   #Background constraint
+fit0.Constrain(1,0.095/sc,0.105/sc)   #Background constraint
 fit0.Constrain(2,0.0,0.9)       #ttg constraint
 fit0.Constrain(3,0.0,0.9)       #ttq constraint
 
@@ -81,8 +87,6 @@ c0 = r.TCanvas("c0","ttj fit",600,400)
 if status == 0:
    result0 = fit0.GetPlot()
    hists["data"].Draw("Ep")
-
-#tf.Close()
 
 #Allows to pass-by-ref to GetResult
 fbg , dfbg = r.Double(0) , r.Double(0) 
@@ -135,10 +139,10 @@ print "fttq from Lhood=", fttq, "+/-", dfttq
 c0.Print(path_name)
 
 #Opens a file for storing fraction values
-f = open("frac_el.txt","a+r")
+f = open("frac_scale.txt","a+r")
 
 #Makes sure to append only for two fractions
-length = len(f.readlines())
+'''length = len(f.readlines())
 if length < 6:
 
     if s1[1] == "el":
@@ -147,13 +151,15 @@ if length < 6:
         f.write("Muon ")
 
     f.write("Fractions\n")
-    table1 = "fttg  = "+str(fttg)+"  +/- "+str(dfttg)+"\n"
-    table2 = "fttq  = "+str(fttq)+" +/- "+str(dfttq)+"\n"
-
-#Writes the two fractions to the file
-    f.write(table1)
-    f.write(table2)
 
 else:
     print "File full"
     exit()
+'''
+table1 = "fttg  = "+str(fttg)+"  +/- "+str(dfttg)+"\n"
+table2 = "fttq  = "+str(fttq)+" +/- "+str(dfttq)+"\n"
+scal = "Scaled by:"+str(sc)+"\n"
+#Writes the two fractions to the file
+f.write(scal)
+f.write(table1)
+f.write(table2)
