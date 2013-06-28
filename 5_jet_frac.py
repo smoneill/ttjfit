a459d8ba86aee5e40fc0d1cf80e9171c1c5c69da
Sean O'Neill <smoneill@frontiernet.net>
2013-06-28 11:02:59 -0400

    Draft using classes needs to be reviewed

--- 5_jet_frac.py
+++ 5_jet_frac.py
@@ -3,16 +3,22 @@
 import ROOT as r
 import sys
 import numpy as np
-import RedConDict as rdc
 
 class program(object):
     def __init__(self,rootFile):
+        '''Calculates the background fraction as well as the background and tt components'''
+
         channelList = self.parseArgument(rootFile)
+
         effDict, preFracDict, eff4_5Dict, signalDict = self.parseData(channelList)
-        self.calculateFractions(effDict, preFracDict, eff4_5Dict, signalDict)
-        self.printFractions()
+
+        backgroundFrac,ttComps,bgComps,tt_5j,bg_5j = self.calculateFractions(effDict, preFracDict, eff4_5Dict, signalDict)
+
+        self.printFractions(backgroundFrac,ttComps,bgComps,tt_5j,bg_5j)
         
     def parseArgument(self,rootFile):
+        '''Makes sure a root file is specified and sets the channel'''
+
         if len(rootFile)<2: 
             print "usage: ttjfit_trans <file.root>"   
             exit() 
@@ -24,17 +30,19 @@ class program(object):
         return self.channelSelect(channel)
 
     def channelSelect(self,ch):
+        '''Selects what columns to read from each file based on the channel'''
+
         if ch == "el":
             num = [0,7,2,10,2,2,7]
         else:
             num = [7,14,11,19,1,2,7]
         name = ["loFrac", "hiFrac", "loCerba", "hiCerba", "column", "loExpect", "hiExpect"]
         return dict([(name[k],num[k]) for k in range(len(num))])
-    
 
-
-
+############################################################################################    
     def parseData(self,channelList):
+        '''Parses the data from three different files and stores them in four dictionaries'''
+        
         fracEffFile = "data/fracs_effs_edit.txt"
         cerbaEffFile = "data/cerba_efficiencies.txt"
         cerbaExpFile = "data/cerba_expect_4j.txt"
@@ -43,20 +51,25 @@ class program(object):
         for item in fileList:
             files.append(self.fileStrip(item))
         
+        #Extracts data from the preselection file
         name = files[0][channelList["loFrac"]:channelList["hiFrac"]]
         eff = self.makeDictionary(name,2)
         preFrac = self.makeDictionary(name,4)
-        
+
+        #Extracts data from the 4->5 efficiency file
         name = files[1][channelList["loCerba"]:channelList["hiCerba"]]
         eff4_5 = self.makeDictionary(name,1)
         
+        #Extracts data from the preselection signal file 
         name = files[2][channelList["loExpect"]:channelList["hiExpect"]]
         signal = self.makeDictionary(name,channelList["column"])
+        
         return eff,preFrac,eff4_5,signal
 
     def fileStrip(self,fileName):
-        with open(fileName) as f:
+        '''Strips the files so they are readable'''
         
+        with open(fileName) as f:
             stripped = [line.strip() for line in f.readlines()]
             translated = [stripped[k].translate(None, ";:") for k in range(len(stripped))]
             if fileName == "data/cerba_efficiencies.txt":
@@ -65,68 +78,60 @@ class program(object):
                 return [translated[k].split() for k in range(len(translated))]
 
     def makeDictionary(self,dic,columnNum):
+        '''Stuffs dictionaries for storing data'''
         return dict([(dic[k][0],float(dic[k][columnNum]))for k in range(len(dic))])
 
-
-
-
+####################################################################################
     def calculateFractions(self,effDict,preFracDict,eff4_5Dict,signalDict):
+        '''Calculates the background fraction as well as background and tt components by applying efficiency filters'''
+        
         comps = ["ttgg","ttqg","ttag","ttqq","wj","st","dy","mj"]
-        ttComps = ["ttgg","ttqg","ttag","ttqq"]
         bgComps = ["wj","st","dy","mj"]
+        ttComps = ["ttgg","ttqg","ttag","ttqq"]
 
+        #Unpacks dictionaries depending on their components
+        bg_4jet = [signalDict[k] for k in bgComps]
         ttPreEff = [effDict[k] for k in ttComps]
         ttPreFrac = [preFracDict[k] for k in ttComps]
-        bg_4jEvents = [signalDict[k] for k in bgComps]
         ttEff4_5 = [eff4_5Dict[k] for k in ttComps]
-
+                
+        bg_4jet[0] += bg_4jet[-1]    #mj gets absorbed into wj                                    
+        bgComps.remove("mj")                                                                                                                  
+        bg_4jet.remove(bg_4jet[-1])                                                                                                   
         
+        bgEff4_5 = [eff4_5Dict[k] for k in bgComps]         
+        
+        #Moves the tt components from preselection->4 jet
+        tt_4jet = self.efficiencyFilter(ttPreEff,ttPreFrac,signalDict["tt"])
+        
+        #Moves both the background and tt components from 4->5 jet
+        tt_5jet = self.efficiencyFilter(tt_4jet,ttEff4_5)
+        bg_5jet = self.efficiencyFilter(bg_4jet,bgEff4_5)
 
-        print zip(ttComps,ttPreEff)
-        print zip(ttComps,ttPreFrac)
-        print zip(ttComps, ttEff4_5)
-        print zip(bgComps,bg_4jEvents)
+        ttTotal = sum(tt_5jet)
+        bgTotal = sum(bg_5jet)
 
+        #Calculates the background fraction
+        bckgrndFrac = bgTotal/(ttTotal+bgTotal)
+        
+        return bckgrndFrac, ttComps, bgComps, bg_5jet, tt_5jet
 
+    def efficiencyFilter(self,vector1,vector2,number=1):
+        '''Filters the events depending on the respective efficiencies, to move them from one selection to another'''
+        piecewiseProduct = [x*y for x,y in zip(vector1,vector2)]
+        dotProduct = np.dot(vector1,vector2)
+        normalizedComponents = [piecewiseProduct[k]/dotProduct for k in range(len(piecewiseProduct))]
+        event = [normalizedComponents[k]*number for k in range(len(normalizedComponents))]
+        if number != 1:
+            return event
+        return piecewiseProduct
 
-
-
-
-    def printFractions(self):
-        print "ya fractions!"
+#####################################################################################
+    def printFractions(self,backgroundFrac,ttComps,bgComps,tt_5jet,bg_5jet):
+        '''Prints the background fraction, background components, and tt components'''
+        print "Background Fraction: %.6f" %backgroundFrac
+        print "Background Components: \n", zip(bgComps,bg_5jet)
+        print "tt Components: \n", zip(ttComps,tt_5jet)
 
 if __name__=='__main__':
     program(sys.argv)
-'''
-#Takes specified comps out of the three dicts
-ttPreEff = [effDict[k] for k in ttComps]
-ttPreFrac = [preFracDict[k] for k in ttComps]
-
-bg_4jEvnts = [signalDict[k] for k in bgComps]
-
-ttEff4_5 = [eff4_5Dict[k] for k in ttComps]
-bg_4jEvnts[0] += bg_4jEvnts[-1]    #mj gets absorbed into wj
-bgComps.remove("mj")
-bg_4jEvnts.remove(bg_4jEvnts[-1])
-bgEff4_5 = [eff4_5Dict[k] for k in bgComps]
-
-tt_4j = rdc.Convert(ttPreEff, ttPreFrac,signalDict["tt"],3)
-
-tt_5j = rdc.Convert(tt_4j, ttEff4_5,1,1)
-bg_5j = rdc.Convert(bg_4jEvnts, bgEff4_5,1,1)
-
-#Total background and tt events for 5-jets
-ttTotal = sum(tt_5j)
-bgTotal = sum(bg_5j)
-
-#Relative background signal for 5-jets
-backgrdFrac = bgTotal/(ttTotal+bgTotal)
-
-tt = dict([(ttComps[items],tt_5j[items]) for items in range(len(tt_5j))])
-bg = dict([(bgComps[k],bg_5j[k]) for k in range(len(bg_5j))])
-print bg["wj"]
-
-print "Background components: \n", zip(bgComps,bg_5j)
-print "tt components: \n", zip(ttComps,tt_5j)
-print "Background fraction: \n",backgrdFrac
-'''
