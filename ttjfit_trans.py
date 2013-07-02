7956629561cd6b1163f23cecd6be6e1c04e52e1c
Sean O'Neill <smoneill@frontiernet.net>
2013-07-02 14:24:28 -0400

    added five_jet_frac module

--- ttjfit_trans.py
+++ ttjfit_trans.py
@@ -1,18 +1,12 @@
 ###ttjfit translation from C++ to PyRoot###
 ###Sean O'Neill###
-
+from five_jet_frac import fiveJetEfficiencyFilter
 import ROOT as r
+r.gROOT.SetBatch(1)
 import sys
-import numpy as np
-import RedConDict as rdc
-
-if len(sys.argv)<2:
-    print "Usage: ttjfit_trans <file.root>"
-    exit()
-else:
-    file_name = sys.argv[1]  
 
 #Where to print to
+file_name = sys.argv[1]
 s = file_name.split(".")
 s1 = s[0].split("_")
 path_name = "graphs/frac_"+s1[1]+".pdf"
@@ -21,74 +15,15 @@ channel = s1[1]
 #Opens ROOT file
 tf = r.TFile.Open(file_name)
 
-#Dictionary of data
+#Dictionary of the template histograms
 hists = dict([(h.GetName(),tf.Get(h.GetName())) for h in tf.GetListOfKeys()])
 
-if channel == "el":
-    lowFracs, highFracs = 0,7
-    lowCerba, highCerba = 2,10
-    column = 2
-else:
-    lowFracs, highFracs = 7,14
-    lowCerba, highCerba = 11,19
-    column = 1
+#Dictionaries of the components of the background and tt 5-jets
+fJetFracs = fiveJetEfficiencyFilter(sys.argv)
+ttDict = dict(zip(fJetFracs.ttComps,fJetFracs.tt_5jet))
+bgDict = dict(zip(fJetFracs.bgComps,fJetFracs.bg_5jet))
 
-lowExpect, highExpect = 2,7
-########################################
-eff_dict = {}       #Dict of 4 jet efficiencies
-frac_pre_dict = {}  #Dict of pre-selection fractions
-eff4_5 = {}         #Dict of 4->5 jet efficiencies
-
-with open("data/fracs_effs_edit.txt") as f:
-
-    name = rdc.FileStrip(f)  
-    name = name[lowFracs:highFracs]
-    effDict = rdc.Dict(name,2)
-    preFracDict = rdc.Dict(name,4)
-
-with open("data/cerba_efficiencies.txt") as ef:
-    name = rdc.FileStrip(ef)
-    name = name[lowCerba:highCerba]
-    eff4_5Dict = rdc.Dict(name,1)
-
-with open("data/cerba_expect_4j.txt") as ex:
-    name = rdc.FileStrip(ex)
-    name = name[lowExpect:highExpect]
-    signalDict = rdc.Dict(name,column)
-
-#Components of background and tt
-comps = ["ttgg","ttqg","ttag","ttqq","wj","st","dy","mj"]
-ttComps = ["ttgg","ttqg","ttag","ttqq"]
-bgComps = ["wj","st","dy","mj"]
-
-#Takes specified comps out of the three dicts
-ttPreEff = [effDict[k] for k in ttComps]
-ttPreFrac = [preFracDict[k] for k in ttComps]
-
-bg_4jEvnts = [signalDict[k] for k in bgComps]
-
-ttEff4_5 = [eff4_5Dict[k] for k in ttComps]
-bg_4jEvnts[0] += bg_4jEvnts[-1]    #mj gets absorbed into wj
-bgComps.remove("mj")
-bg_4jEvnts.remove(bg_4jEvnts[-1])
-bgEff4_5 = [eff4_5Dict[k] for k in bgComps]
-
-tt_4j = rdc.Convert(ttPreEff, ttPreFrac,signalDict["tt"],3)
-
-tt_5j = rdc.Convert(tt_4j, ttEff4_5,1,1)
-bg_5j = rdc.Convert(bg_4jEvnts, bgEff4_5,1,1)
-
-#Total background and tt events for 5-jets
-ttTotal = sum(tt_5j)
-bgTotal = sum(bg_5j)
-
-#Relative background signal for 5-jets
-backgrdFrac = bgTotal/(ttTotal+bgTotal)
-
-ttDict = dict([(ttComps[items],tt_5j[items]) for items in range(len(tt_5j))])
-bgDict = dict([(bgComps[k],bg_5j[k]) for k in range(len(bg_5j))])
-
-#Stores number of events
+#Total number of events
 ndata = hists["data"].Integral()
 
 #Creates histogram and fills them from file, efficiencies from file
@@ -105,10 +40,6 @@ ttg1.Add(hists["ttgg"],hists["ttqq"],ttDict["ttgg"],ttDict["ttqq"])
 ttq1 = r.TH1F("ttq1","ttq1",50,0,0.11)
 ttq1.Add(hists["ttqg"],hists["ttag"],ttDict["ttqg"],ttDict["ttag"]) 
 
-ndata1 = bg.Integral()
-ndata1 += ttg.Integral()
-ndata1 += ttq.Integral()
-
 #Scales to large number to reduce template uncertainties
 bg.Scale(1000000.)
 ttg.Scale(1000000.)
@@ -123,32 +54,34 @@ mc80.Add(ttq)
 
 #Initializes the fitter
 fit0 = r.TFractionFitter(hists["data"],mc80)
-fit0.Constrain(1,backgrdFrac-0.005,0.005+backgrdFrac)   #Background constraint
+fit0.Constrain(1,fJetFracs.backgroundFrac-0.005,0.005+fJetFracs.backgroundFrac)   #Background constraint
 fit0.Constrain(2,0.0,0.9)       #ttg constraint
 fit0.Constrain(3,0.0,0.9)       #ttq constraint
 
+#Opens a canvas to print the histograms 
 status = fit0.Fit()
 c0 = r.TCanvas("c0","ttj fit",600,400)
 
 #Allows to pass-by-ref to GetResult
-fbg , dfbg,=  r.Double(0) , r.Double(0) 
+fbg , dfbg =  r.Double(0) , r.Double(0) 
+fttg , dfttg = r.Double(0) , r.Double(0)
+fttq , dfttq = r.Double(0) , r.Double(0)
 
 #Gets results from the fitting
 fit0.GetResult(0,fbg,dfbg)
-scale = fbg#*ndata
-bg1.Scale(scale)                        #scales bg1
-
-fttg , dfttg = r.Double(0) , r.Double(0)
 fit0.GetResult(1,fttg,dfttg)
-scale = fttg#*ndata
-ttg1.Scale(scale)                       #scales ttg1
+fit0.GetResult(2,fttq,dfttq)
+
+def scaling(histogram, frac):
+    histogram.Scale(frac*ndata/histogram.Integral()) 
+
+scaling(bg1,fbg)
+scaling(ttg1,fttg)
+scaling(ttq1,fttq)
+
 sm1 = r.TH1F("sm1", "sm1",50,0,0.11)    #histogram sm1
 sm1.Add(bg1,ttg1,1.,1.)                 #fills sm1
 
-fttq , dfttq = r.Double(0) , r.Double(0)
-fit0.GetResult(2,fttq,dfttq)
-scale = fttq#*ndata
-ttq1.Scale(scale)                       #scales ttq1
 sm2 = r.TH1F("sm2","sm2",50,0,0.11)     #histogram sm2
 sm2.Add(sm1,ttq1,1.,1.)                 #fills sm2
 
@@ -156,10 +89,6 @@ sm2.Add(sm1,ttq1,1.,1.)                 #fills sm2
 sm2.SetFillColor(r.kBlue)
 sm1.SetFillColor(r.kRed)
 bg1.SetFillColor(r.kGreen+3)
-
-ndata2 = sm2.Integral()
-
-print "Data Integral " ,ndata , "\n", "Total Result Integral ", ndata2
 
 #Draws the three fits created with c0 on one canvas
 sm2.Draw("HIST Same")                        #draws sm1
@@ -187,8 +116,10 @@ if s1[1] == "el":
 if s1[1] == "mu":
     print "Muon Channel"
 
+print "Sum Fraction =", sum([fttg,fttq,fbg])
 print "fttg from Lhood=", fttg, "+/-", dfttg
 print "fttq from Lhood=", fttq, "+/-", dfttq
+print "fttq/(fttg+fttq)=", fttq/sum([fttq,fttg])
 
 #Prints histograms to file                                            
 c0.Print(path_name)
